e39905c33e060b510a49817941f774272f8554ac
Benny Powers <web@bennypowers.com>
2021-11-21 10:19:09 +0200

    update install script

--- .cfg-install.sh
+++ .cfg-install.sh
@@ -5,12 +5,13 @@ function config {
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
-mkdir -p .config-backup
 config checkout
+
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
+  mkdir -p .config-backup/.config
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 
