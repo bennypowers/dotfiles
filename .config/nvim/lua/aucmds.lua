20e12b41784976c7acec84aa851c3f70b92a53fb
Benny Powers <web@bennypowers.com>
2022-05-31 10:58:22 +0300

    style: nicer aucmds

--- .config/nvim/lua/aucmds.lua
+++ .config/nvim/lua/aucmds.lua
@@ -1,8 +1,12 @@
 local U = require 'utils'
 
-vim.api.nvim_create_augroup('help_options', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-  group = 'help_options',
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+---Disable cursor column in help buffers
+--
+autocmd({ 'BufEnter', 'BufWinEnter' }, {
+  group = augroup('help_options', { clear = true }),
   pattern = '*',
   callback = function()
     if vim.bo.filetype == 'help' then
@@ -11,25 +15,28 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   end
 })
 
-vim.api.nvim_create_augroup('nunjucks_ft', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  group = 'nunjucks_ft',
+---Apply the 'jinja' file type to nunjucks files
+--
+autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = augroup('nunjucks_ft', { clear = true }),
   pattern = '*.njk',
   command = 'set ft=jinja',
 })
 
-vim.api.nvim_create_augroup('packer_user_config', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = 'packer_user_config',
-  pattern = { 'plugins.lua', 'init.lua' },
+---Clean and compile packer when `plugins.lua` or `init.lua` change
+--
+autocmd('BufWritePost', {
+  group = augroup('packer_user_config', { clear = true }),
+  pattern = { 'plugins.lua', 'init.lua', 'lua/config/*.lua' },
   callback = U.refresh_packer,
 })
 
 
-vim.api.nvim_create_augroup('LspFormatting', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
+---Format on save, using only the clients with `autoFixOnSave` set
+--
+autocmd('BufWritePre', {
+  group = augroup('LspFormatting', { clear = true }),
   pattern = '*',
-  group = 'LspFormatting',
   callback = function()
     vim.lsp.buf.format {
       timeout_ms = 2000,
@@ -40,28 +47,34 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end
 })
 
-vim.api.nvim_create_augroup('alpha_on_empty', { clear = true })
-vim.api.nvim_create_autocmd('User', {
+---For reasons unclear to me, eslint ls doesn't autoFixOnSave, so execute `EslineFixAll` instead
+--
+autocmd('BufWritePre', {
+  group = augroup('eslint-fixall', { clear = true }),
+  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js', },
+  command = 'EslintFixAll',
+})
+
+---Any time a buffer closes, when there are no more active buffers, Launch Alpha
+--
+autocmd('User', {
   pattern = 'BDeletePre',
-  group = 'alpha_on_empty',
+  group = augroup('alpha_on_empty', { clear = true }),
   callback = function(event)
-    local found_non_empty_buffer = false
-    local buffers = U.get_listed_buffers()
-
-    for _, bufnr in ipairs(buffers) do
-      if not found_non_empty_buffer then
-        local name = vim.api.nvim_buf_get_name(bufnr)
-        local ft = vim.api.nvim_buf_get_option(bufnr, 'filetype')
-
-        if bufnr ~= event.buf and name ~= '' and ft ~= 'Alpha' then
-          found_non_empty_buffer = true
-        end
-      end
-    end
-
-    if not found_non_empty_buffer then
-      require 'neo-tree'.close_all()
-      vim.cmd [[:Alpha]]
+    if not U.has_non_empty_buffers(event.buf) then
+      vim.cmd [[ :Alpha ]]
     end
   end,
+})
+
+---Launch alpha if vim starts with only empty buffers
+--
+autocmd('VimEnter', {
+  group = augroup('alpha_on_startup', { clear = false }),
+  pattern = '*',
+  callback = function()
+    if not U.has_non_empty_buffers() and not require 'session_manager.utils'.get_last_session_filename() then
+      vim.cmd [[ :Alpha ]]
+    end
+  end
 })
