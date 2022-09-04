06975847375d9c7a430433466f4552522afab420
Benny Powers <web@bennypowers.com>
2022-09-04 19:46:13 +0300

    fish: greeeeeen

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -6,53 +6,39 @@ set -g fish_user_paths "~/.yarn/bin" $fish_user_paths
 
 status --is-interactive; and source (rbenv init -|psub)
 
-# override btf for github dark theme
+# override btf for to use terminal colors
 function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthefish'
-  # GitHub Dark
-  set -l cyan         39c5cf
-  set -l green        3fb950
-  set -l orange       ffa657
-  set -l pink         bc8ccf
-  set -l red          ff7b72
-  set -l yellow       d29922
-  set -l bg           0d1117
-  set -l selection    163356
-  set -l fg           4d5566
-  set -l comment      8b949e
-  set -l purple       bd93f9
-  set -l current_line 44475a
+  set -x color_initial_segment_exit     brwhite red  --bold
+  set -x color_initial_segment_private  brwhite white
+  set -x color_initial_segment_su       brwhite purple --bold
+  set -x color_initial_segment_jobs     brwhite brblack --bold
 
-  set -x color_initial_segment_exit     $fg $red  --bold
-  set -x color_initial_segment_private  $fg $selection
-  set -x color_initial_segment_su       $fg $purple --bold
-  set -x color_initial_segment_jobs     $fg $comment --bold
+  set -x color_path                     blue brwhite
+  set -x color_path_basename            blue brwhite --bold
+  set -x color_path_nowrite             blue red
+  set -x color_path_nowrite_basename    blue red --bold
 
-  set -x color_path                     $selection $fg
-  set -x color_path_basename            $selection $fg --bold
-  set -x color_path_nowrite             $selection $red
-  set -x color_path_nowrite_basename    $selection $red --bold
+  set -x color_repo                     green black
+  set -x color_repo_work_tree           blue brwhite --bold
+  set -x color_repo_dirty               red black
+  set -x color_repo_staged              bryellow black
 
-  set -x color_repo                     $green $bg
-  set -x color_repo_work_tree           $selection $fg --bold
-  set -x color_repo_dirty               $red $bg
-  set -x color_repo_staged              $yellow $bg
+  set -x color_vi_mode_default          black bryellow --bold
+  set -x color_vi_mode_insert           green black --bold
+  set -x color_vi_mode_visual           yellow black --bold
 
-  set -x color_vi_mode_default          $bg $yellow --bold
-  set -x color_vi_mode_insert           $green $bg --bold
-  set -x color_vi_mode_visual           $orange $bg --bold
-
-  set -x color_vagrant                  $pink $bg --bold
-  set -x color_k8s                      $purple $bg --bold
-  set -x color_aws_vault                $comment $yellow --bold
-  set -x color_aws_vault_expired        $comment $red --bold
-  set -x color_username                 $selection $cyan --bold
-  set -x color_hostname                 $selection $cyan
-  set -x color_rvm                      $red $bg --bold
-  set -x color_node                     $green $bg --bold
-  set -x color_virtualfish              $comment $bg --bold
-  set -x color_virtualgo                $cyan $bg --bold
-  set -x color_desk                     $comment $bg --bold
-  set -x color_nix                      $cyan $bg --bold
+  set -x color_vagrant                  brmagenta black --bold
+  set -x color_k8s                      magenta black --bold
+  set -x color_aws_vault                brblack bryellow --bold
+  set -x color_aws_vault_expired        brblack red --bold
+  set -x color_username                 blue cyan --bold
+  set -x color_hostname                 blue cyan
+  set -x color_rvm                      red black --bold
+  set -x color_node                     green black --bold
+  set -x color_virtualfish              brblack black --bold
+  set -x color_virtualgo                cyan black --bold
+  set -x color_desk                     brblack black --bold
+  set -x color_nix                      cyan black --bold
 end
 
 # My Additions
@@ -65,7 +51,8 @@ alias rn="npm run"
 alias realvim="vim"
 alias oldvim="vim"
 alias bramvim="vim"
-alias vim="env TERM=wezterm nvim"
+# alias vim="env TERM=wezterm nvim"
+alias vim="nvim"
 alias vim-update="nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
 alias config="git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
