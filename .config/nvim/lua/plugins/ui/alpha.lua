14b5872affd8025822daa19a66a90aa030f4a22d
Benny Powers <web@bennypowers.com>
2023-02-21 12:55:01 +0200

    nvim: refactor alpha stuff

--- .config/nvim/lua/plugins/ui/alpha.lua
+++ .config/nvim/lua/plugins/ui/alpha.lua
@@ -5,154 +5,38 @@ return { 'goolord/alpha-nvim',
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    math.randomseed(os.time(os.date'!*t'))
-
-    local path = require 'plenary.path'
-    local nvim_web_devicons = require 'nvim-web-devicons'
+    local mru = require'utils'.mru
     local alpha = require 'alpha'
     local term = require 'alpha.term'
     local dashboard = require 'alpha.themes.dashboard'
-
-    local cdir = vim.fn.getcwd()
-
-    local function get_extension(fn)
-      local match = fn:match("^.+(%..+)$")
-      local ext = ""
-      if match ~= nil then
-        ext = match:sub(2)
-      end
-      return ext
-    end
-
-    local function icon(fn)
-      local ext = get_extension(fn)
-      return nvim_web_devicons.get_icon(fn, ext, { default = true })
-    end
-
-    local function file_button(fn, sc, short_fn)
-      short_fn = short_fn or fn
-      local ico_txt
-      local fb_hl = {}
-
-      local ico, hl = icon(fn)
-      local hl_option_type = type(nvim_web_devicons.highlight)
-
-      if hl_option_type == "boolean" then
-        if hl and nvim_web_devicons.highlight then
-          table.insert(fb_hl, { hl, 0, 1 })
-        end
-      end
-
-      if hl_option_type == "string" then
-        table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 1 })
-      end
-
-      ico_txt = ico .. "  "
-
-      local file_button_el = dashboard.button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. " <CR>")
-      local fn_start = short_fn:match(".*/")
-      if fn_start ~= nil then
-        table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt - 2 })
-      end
-      file_button_el.opts.hl = fb_hl
-      return file_button_el
-    end
-
-    local default_mru_ignore = { "gitcommit" }
-
-    local mru_opts = {
-      ignore = function(filepath, ext)
-        return (string.find(filepath, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
-      end,
-    }
-
-    --- @param start number
-    --- @param cwd string optional
-    --- @param items_number number optional number of items to generate, default = 10
-    local function mru(start, cwd, items_number, opts)
-      opts = opts or mru_opts
-      items_number = items_number or 9
-
-      local oldfiles = {}
-      for _, v in pairs(vim.v.oldfiles) do
-        if #oldfiles == items_number then
-          break
-        end
-        local cwd_cond
-        if not cwd then
-          cwd_cond = true
-        else
-          cwd_cond = vim.startswith(v, cwd)
-        end
-        local ignore = (opts.ignore and opts.ignore(v, get_extension(v))) or false
-        if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
-          oldfiles[#oldfiles + 1] = v
-        end
-      end
-
-      local special_shortcuts = { 'a', 's', 'd' }
-      local target_width = 35
-
-      local tbl = {}
-      for i, fn in ipairs(oldfiles) do
-        local short_fn
-        if cwd then
-          short_fn = vim.fn.fnamemodify(fn, ":.")
-        else
-          short_fn = vim.fn.fnamemodify(fn, ":~")
-        end
-
-        if (#short_fn > target_width) then
-          short_fn = path.new(short_fn):shorten(1, { -2, -1 })
-          if (#short_fn > target_width) then
-            short_fn = path.new(short_fn):shorten(1, { -1 })
-          end
-        end
-
-        local shortcut = ""
-        if i <= #special_shortcuts then
-          shortcut = special_shortcuts[i]
-        else
-          shortcut = tostring(i + start - 1 - #special_shortcuts)
-        end
-
-        local file_button_el = file_button(fn, " " .. shortcut, short_fn)
-        tbl[i] = file_button_el
-      end
-
-      return {
-        type = "group",
-        val = tbl,
-        opts = {},
-      }
-    end
+    local nvim_web_devicons = require 'nvim-web-devicons'
 
     local width = 16
     local height = 16
 
-    local headers_cat = '16'
-
-    -- local winwidth = vim.fn.winwidth'%'
-    -- local winheight = vim.fn.winheight'%'
-    --
-    -- if winwidth >= 100 and winheight >= 60 then
-    --   headers_cat = 'small'
-    --   height = 25
-    -- end
-    --
-    -- if winwidth >= 100 and winheight >= 60 then
-    --   headers_cat = 'wide'
-    --   width = 100
-    -- end
-    --
-    -- if winheight >= 60 then
-    --   headers_cat = 'tall'
-    --   height = 40
-    -- end
-
-    local headers_dirname = vim.fn.expand('~/.config/nvim/headers/' .. headers_cat)
-
     local function get_header_script()
+      local headers_cat = '16'
+
+      -- local winwidth = vim.fn.winwidth'%'
+      -- local winheight = vim.fn.winheight'%'
+      --
+      -- if winwidth >= 100 and winheight >= 60 then
+      --   headers_cat = 'small'
+      --   height = 25
+      -- end
+      --
+      -- if winwidth >= 100 and winheight >= 60 then
+      --   headers_cat = 'wide'
+      --   width = 100
+      -- end
+      --
+      -- if winheight >= 60 then
+      --   headers_cat = 'tall'
+      --   height = 40
+      -- end
+      --
+      local headers_dirname = vim.fn.expand('~/.config/nvim/headers/' .. headers_cat)
+
       local headers = {}
 
       for _, filename in ipairs(vim.fn.readdir(headers_dirname)) do
@@ -160,22 +44,19 @@ return { 'goolord/alpha-nvim',
       end
 
       local idx = math.random(1, #headers)
-      return headers[idx]
+      return 'cat | ' .. headers[idx]
     end
 
     alpha.setup {
       layout = {
         {
-          type = "terminal",
-          command = 'cat | ' .. get_header_script(),
+          type = 'terminal',
+          command = get_header_script,
           width = width,
           height = height,
           opts = {
             redraw = true,
             window_config = {
-              width = width,
-              height = height,
-              style = 'minimal',
               noautocmd = true,
             },
           },
@@ -184,22 +65,51 @@ return { 'goolord/alpha-nvim',
         { type = 'padding', val = height },
 
         {
-          type = "group",
+          type = 'group',
           val = {
             {
-              type = "text",
-              val = "Recent files",
+              type = 'text',
+              val = 'Recent files',
               opts = {
-                hl = "SpecialComment",
+                hl = 'SpecialComment',
                 shrink_margin = false,
-                position = "center",
+                position = 'center',
               },
             },
             { type = 'padding', val = 1 },
             {
-              type = "group",
+              type = 'group',
               val = function()
-                return { mru(1, cdir, 5) }
+                return {
+                  {
+                    type = 'group',
+                    val = vim.tbl_map(function(x)
+                      local filename, key_label, short_filename = unpack(x)
+                      local final_filename = short_filename or filename
+                      local icon_ext = ''
+                      local match = filename:match'^.+(%..+)$' if match then icon_ext = match:sub(2) end
+                      local ico, hl = nvim_web_devicons.get_icon(filename, icon_ext, { default = true })
+                      local fb_hl = {}
+                      local hl_option_type = type(nvim_web_devicons.highlight)
+                      if hl_option_type == 'boolean' then
+                        if hl and nvim_web_devicons.highlight then
+                          table.insert(fb_hl, { hl, 0, 1 })
+                        end
+                      elseif hl_option_type == 'string' then
+                        table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 1 })
+                      end
+                      local file_button_el =
+                        dashboard.button(key_label, ico .. '  ' .. final_filename, '<cmd>e ' .. filename .. ' <CR>')
+                      local filename_start = final_filename:match'.*/'
+                      if filename_start ~= nil then
+                        table.insert(fb_hl, { 'Comment', #ico, #filename_start + #ico })
+                      end
+                      file_button_el.opts.hl = fb_hl
+                      return file_button_el
+                    end, mru(1, 5, vim.fn.getcwd())),
+                    opts = {},
+                  }
+                }
               end,
               opts = { shrink_margin = false },
             },
@@ -209,17 +119,17 @@ return { 'goolord/alpha-nvim',
         { type = 'padding', val = 2 },
 
         {
-          type = "group",
+          type = 'group',
           val = {
-            { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+            { type = 'text', val = 'Quick links', opts = { hl = 'SpecialComment', position = 'center' } },
             { type = 'padding', val = 1 },
-            dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("spc /", "  File Explorer", ":Neotree float filesystem<CR>"),
-            dashboard.button("spc p", "  Find file", ":Telescope find_files <CR>"),
-            dashboard.button("spc fg", "  Find text", ":Telescope live_grep <CR>"),
-            dashboard.button("q", "  Quit", ":qa<CR>"),
+            dashboard.button('n', '  New file', ':ene <BAR> startinsert <CR>'),
+            dashboard.button('spc /', '  File Explorer', ':Neotree float filesystem<CR>'),
+            dashboard.button('spc p', '  Find file', ':Telescope find_files <CR>'),
+            dashboard.button('spc fg', '  Find text', ':Telescope live_grep <CR>'),
+            dashboard.button('q', '  Quit', ':qa<CR>'),
           },
-          position = "center",
+          position = 'center',
         },
       },
       opts = {
--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -40,4 +40,73 @@ function M.open_uri_under_cursor()
   vim.api.nvim_feedkeys('O', 'n', false)
 end
 
+
+--- Most Recently Used files
+--- @param start number
+--- @param items_number number optional number of items to generate, default = 10
+--- @param cwd string optional
+function M.mru(start, items_number, cwd, opts)
+  local cont, path = pcall(require,'plenary.path')
+  if not cont then return {} end
+
+  opts = opts or {
+    ignore = function(filepath, ext)
+      return (string.find(filepath, "COMMIT_EDITMSG")) or (vim.tbl_contains({ "gitcommit" }, ext))
+    end,
+  }
+
+  items_number = items_number or 9
+
+  local oldfiles = {}
+  for _, v in pairs(vim.v.oldfiles) do
+    if #oldfiles == items_number then
+      break
+    end
+    local cwd_cond
+    if not cwd then
+      cwd_cond = true
+    else
+      cwd_cond = vim.startswith(v, cwd)
+    end
+    local ignore_ext = ''
+    local match = v:match'^.+(%..+)$' if match then ignore_ext = match:sub(2) end
+    local ignore = (opts.ignore and opts.ignore(v, ignore_ext)) or false
+    if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
+      oldfiles[#oldfiles + 1] = v
+    end
+  end
+
+  local special_shortcuts = { 'a', 's', 'd' }
+  local target_width = 35
+
+  local tbl = {}
+  for i, file_name in ipairs(oldfiles) do
+    local short_file_name
+    if cwd then
+      short_file_name = vim.fn.fnamemodify(file_name, ":.")
+    else
+      short_file_name = vim.fn.fnamemodify(file_name, ":~")
+    end
+
+    if (#short_file_name > target_width) then
+      short_file_name = path.new(short_file_name):shorten(1, { -2, -1 })
+      if (#short_file_name > target_width) then
+        short_file_name = path.new(short_file_name):shorten(1, { -1 })
+      end
+    end
+
+    local shortcut = ""
+    if i <= #special_shortcuts then
+      shortcut = special_shortcuts[i]
+    else
+      shortcut = tostring(i + start - 1 - #special_shortcuts)
+    end
+
+    tbl[i] = { file_name, " " .. shortcut, short_file_name }
+  end
+
+  return tbl
+end
+
+
 return M
