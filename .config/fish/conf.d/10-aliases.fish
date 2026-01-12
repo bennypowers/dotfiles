8597b04524e79b1ce77988fefa9a8e69064f49b6
Benny Powers <web@bennypowers.com>
2026-01-12 15:36:58 +0200

    fish: git alias

--- .config/fish/conf.d/10-aliases.fish
+++ .config/fish/conf.d/10-aliases.fish
@@ -1,5 +1,6 @@
 # aliases
 alias c="clear; and source ~/.config/fish/config.fish; and colorscript --random"
+alias g="git"
 alias config="git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias lg="lazygit"
 alias lgc="lazygit -g ~/.cfg -w ~ -ucf $HOME/.config/lazygit/dotfiles.yml,$HOME/.config/lazygit/config.yml"
